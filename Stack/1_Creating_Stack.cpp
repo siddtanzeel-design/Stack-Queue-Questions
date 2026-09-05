be93e022a8ed78457308f55d4648a0283c98f2d9
Md Tanzeel Siddiqui <siddtanzeel@gmail.com>
2026-09-05 19:00:12 +0530

    Create 1_Creating_Stack.cpp

---
+++ Stack/1_Creating_Stack.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+using namespace std;
+
+class Stack
+{
+private:
+    int *arr;
+    int top;
+    int size;
+
+public:
+    Stack(int n)
+    {
+        size = n;
+        top = -1;
+        arr = new int[size];
+    }
+    bool push(int val)
+    {
+        if(top == size - 1)
+        {
+            cout<<"Stack Overflow"<<endl;
+            return false;
+        }
+
+        top++;
+        arr[top] = val;
+
+        return true;
+    }
+    bool pop()
+    {
+        if(top == -1)
+        {
+            cout<<"Stack Underflow"<<endl;
+            return false;
+        }
+
+        top--;
+        return true;
+    }
+    bool peek()
+    {
+        if(top == -1)
+        {
+            cout<<"Stack is empty"<<endl;
+            return false;
+        }
+
+        cout<<arr[top]<<endl;
+        return true;
+    }
+    void display()
+    {
+        if(top == -1)
+        {
+            cout<<"Stack is Empty"<<endl;
+            return;
+        }
+        for(int i = top; i >= 0; i--)
+        {
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+};
+int main()
+{
+    int n;
+
+    cout<<"Enter size of the stack: ";
+    cin>>n;
+
+    Stack s(n);
+
+    char op = 'y';
+
+    do
+    {
+        int choice;
+
+        cout<<"\nChoose your Option"<<endl;
+        cout<<"1. Insert"<<endl;
+        cout<<"2. Delete"<<endl;
+        cout<<"3. Peek"<<endl;
+        cout<<"4. Display"<<endl;
+        cin>>choice;
+
+        if(choice == 1)
+        {
+            int val;
+
+            cout<<"Enter value to insert: ";
+            cin>>val;
+
+            if(s.push(val))
+                cout<<"Value Inserted!"<<endl;
+        }
+        else if(choice == 2)
+        {
+            if(s.pop())
+                cout<<"Value Deleted"<<endl;
+        }
+        else if(choice == 3)
+        {
+            if(s.peek())
+                cout<<"Element at the top"<<endl;
+        }
+        else if(choice == 4)
+            s.display();
+        else
+            cout<<"Invalid Input"<<endl;
+        
+        cout<<"Do you want to Operate again? (y/n): ";
+        cin>>op;
+
+    } while(op == 'y' || op == 'Y');
+    cout<<"Thank You!";
+    return 0;
+}
